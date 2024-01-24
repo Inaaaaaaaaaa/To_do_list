be89b1f1092883ddab6436ce46ca58fb7bcb6f86
Ina <inayumi7@gmail.com>
2024-01-24 14:50:25 +1300

    Updates
    - Adding add and remove button
    
    - Creating a model for list
    
    - Changing size of frame

--- src/todolist/Todolist.java
+++ src/todolist/Todolist.java
@@ -10,6 +10,10 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class Todolist extends JFrame {
+    
+    private JList<String> list;
+    private JTextField inputfield;
+    private DefaultListModel<String> model;
 
     /**
      * @param args the command line arguments
@@ -27,8 +31,27 @@ public class Todolist extends JFrame {
         super("To do list");
         
         //size of frame
-        this.setSize(600,600);
+        this.setSize(650,600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        
+        //input and buttons 
+        JPanel input = new JPanel();
+        inputfield = new JTextField(30);
+        JButton add = new JButton("Add task");
+        JButton remove = new JButton("Remove task");
+        
+        //model for list 
+        model = new DefaultListModel<>();
+        list = new JList<>(model);
+        JScrollPane scroll = new JScrollPane(list);
+        this.add(scroll, BorderLayout.CENTER);
+        
+        //add components 
+        input.add(inputfield);
+        input.add(add);
+        input.add(remove);
+        
+        this.add(input, BorderLayout.NORTH);
     }
 }
